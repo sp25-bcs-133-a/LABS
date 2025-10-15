dead75cfbcc8f37de38d57616c74fdd3fb8d4bfa
sp25-bcs-133-a <sp25-bcs-133@cuilahore.edu.pk>
2025-10-15 17:16:02 +0500

    Add files via upload

---
+++ LAB 6/HouseDemo.java
@@ -0,0 +1,6 @@
+public class HouseDemo{
+	public static void main(String[] args){
+		Plot p1 = new Plot(Type.RES_5_MARLA, true);
+		System.out.println(p1);
+	}
+}
---
+++ LAB 6/run.bat
@@ -0,0 +1,5 @@
+javac -d out src\Plot.java src\Type.java
+
+javac -cp out HouseDemo.java
+
+java -cp out;. HouseDemo
